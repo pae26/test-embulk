22dfb5cc5f82c6120ffa6a2bdc41b087363432aa
Pae <songju19990426@gmail.com>
2023-03-28 22:40:12 +0900

    first commit

---
+++ migrate/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "DBをセットアップしています..."
+MYSQL_PWD="${MYSQL_PASSWORD}" mysql -u "${MYSQL_USER}" -P "${MYSQL_PORT}" -h "${MYSQL_HOST}" -e "DROP database if exists ${MYSQL_DB}; CREATE database ${MYSQL_DB}:"
+
+/usr/local/bin/embulk run migrate-sqlserver-to-mysql.yml.liquid
---
+++ mysql/init/init.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `users_mysql` (
+    `id_mysql` INT PRIMARY KEY,
+    `name_mysql` VARCHAR(100) NOT NULL,
+    `gender_mysql` VARCHAR(10) NOT NULL,
+)
